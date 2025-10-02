907d7af6649e7357c778e5e28450a1b9164a5ccc
nisreen.melhem <nisreen.melhem@hnndes.com>
2025-10-02 16:20:30 +0300

    * connect with emailjs * update profile image

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,69 +1,47 @@
-import React, { useState } from "react";
+import React, {useRef, useState } from "react";
 import axios from "axios";
 import { FiSend } from "react-icons/fi";
 import Title from "../home/Title";
-
+import emailjs from '@emailjs/browser';
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
 const Contact = () => {
-  const [clientName, setClientName] = useState("");
-  const [email, setEmail] = useState("");
-  const [messages, setMessages] = useState("");
 
-  // ================= Error Messages Start here =================
-  const [errClientName, setErrClientName] = useState(false);
-  const [errEmail, setErrEmail] = useState(false);
-  const [errMessages, setErrMessage] = useState(false);
-  // ================= Error Messages End here ===================
-  const [seuccessMsg, setSuccessMsg] = useState("");
-  // ================= Email Validation Start here ===============
-  const EmailValidation = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
-  };
-  // ================= Email Validation End here =================
+  // Send Email
+  const formRef = useRef()
+  const [error, setError] = useState(null)
+  const [success, setSuccess] = useState(null)
+  const [errMsg, setErrMsg] = useState('')
+  const [isLoading, setIsLoading] = useState(false);
+  const sendEmail = (e) => {
+        e.preventDefault();
+        setIsLoading(true);
 
-  const handleName = (e) => {
-    setClientName(e.target.value);
-    setErrClientName(false);
-  };
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-    setErrEmail(false);
-  };
-  const handleMessages = (e) => {
-    setMessages(e.target.value);
-    setErrMessage(false);
-  };
-
-  const handleSend = (e) => {
-    e.preventDefault();
-    if (!clientName) {
-      setErrClientName(true);
-    }
-    if (!email) {
-      setErrEmail(true);
-    } else {
-      if (!EmailValidation(email)) {
-        setErrEmail(true);
-      }
-    }
-    if (!messages) {
-      setErrMessage(true);
-    }
-    if (clientName && email && EmailValidation(email) && messages) {
-      axios.post("https://getform.io/f/e18ee560-5133-4cfe-9a48-eddb6f012a9f", {
-        name: clientName,
-        email: email,
-        message: messages,
-      });
-      setSuccessMsg(
-        `Hello dear ${clientName}, Your messages has been sent successfully. Thank you for your time!`
-      );
-      setClientName("");
-      setEmail("");
-      setMessages("");
-    }
-  };
+        emailjs
+            .sendForm('service_j0v4y5h', 'template_tvuqpyd', formRef.current, {
+                publicKey: 'NIRbnv1lNl6ULxIlU',
+            })
+            .then(
+                () => {
+                    setError(false)
+                    setIsLoading(false); 
+                    setSuccess(true)
+                    const nameInput = document.getElementsByName('name')[0];
+                    const emailInput = document.getElementsByName('email')[0];
+                    const messageInput = document.getElementsByName('message')[0];
+            
+                    nameInput.value = '';
+                    emailInput.value = '';
+                    messageInput.value = '';
+                },
+                (error) => {
+                    setError(true)
+                    setErrMsg('An error occurred while sending the message!!!')
+                    setIsLoading(false); 
+                    setSuccess(false)
+                },
+                
+            );
+    };
   return (
     <div className="w-full">
       <Title title="Get" subTitle="in Touch" />
@@ -99,62 +77,45 @@ const Contact = () => {
       </div>
       <div className="w-full mt-10">
         <Title title="Send" subTitle="Messages" />
-        {seuccessMsg ? (
+        {success ? (
           <p className="text-center text-base font-titleFont p-20 text-designColor">
-            {seuccessMsg}
+    Your message was sent successfully.
           </p>
-        ) : (
+        ) :
+          error ? (
+          <p className="text-center text-base font-titleFont p-20 text-red-600">
+            {errMsg}
+          </p>
+        )
+        : (
           <form
             id="form"
-            action="https://getform.io/f/e18ee560-5133-4cfe-9a48-eddb6f012a9f"
-            method="POST"
+           ref={formRef} onSubmit={sendEmail}
             className="p-6 flex flex-col gap-6"
           >
             <div className="w-full flex flex-col lgl:flex-row gap-4 lgl:gap-10 justify-between">
               <input
-                onChange={handleName}
-                value={clientName}
-                className={`${
-                  errClientName
-                    ? "border-red-600 focus-visible:border-red-600"
-                    : "border-zinc-600 focus-visible:border-designColor"
-                } w-full bg-transparent border-2 px-4 py-2 text-base text-gray-200 outline-none duration-300`}
+                type='text' placeholder='Name' name='name' required 
+                className={` border-zinc-600 focus-visible:border-designColor
+                 w-full bg-transparent border-2 px-4 py-2 text-base text-gray-200 outline-none duration-300`}
                 // className="w-full bg-transparent border-2 px-4 py-2 text-base text-gray-200 border-zinc-600 focus-visible:border-designColor outline-none duration-300"
-                type="text"
-                placeholder="Full Name"
               />
               <input
-                onChange={handleEmail}
-                value={email}
-                className={`${
-                  errEmail
-                    ? "border-red-600 focus-visible:border-red-600"
-                    : "border-zinc-600 focus-visible:border-designColor"
-                } w-full bg-transparent border-2 px-4 py-2 text-base text-gray-200 outline-none duration-300`}
-                type="email"
-                placeholder="Email Address"
+                type='email' placeholder='Email' name='email' required
+                className={` border-zinc-600 focus-visible:border-designColor  w-full bg-transparent border-2 px-4 py-2 text-base text-gray-200 outline-none duration-300`}
               />
             </div>
             <textarea
-              onChange={handleMessages}
-              value={messages}
-              className={`${
-                errMessages
-                  ? "border-red-600 focus-visible:border-red-600"
-                  : "border-zinc-600 focus-visible:border-designColor"
-              } w-full bg-transparent border-2 px-4 py-2 text-base text-gray-200 outline-none duration-300 resize-none`}
-              placeholder="Your Message"
-              rows="4"
+              rows='4' placeholder='Message' name='message' required
+              className={`border-zinc-600 focus-visible:border-designColor w-full bg-transparent border-2 px-4 py-2 text-base text-gray-200 outline-none duration-300 resize-none`}
             ></textarea>
             <button
-              onClick={handleSend}
+             type='submit'  aria-label="submit"
               className="text-base w-44 flex items-center gap-1 text-gray-200 hover:text-designColor duration-200"
             >
-              Send Message{" "}
-              <span className="mt-1 text-designColor">
-                <FiSend />
-              </span>
+              {isLoading?AiOutlineLoading3Quarters:<span className="flex flex-row gap-4 items-center justify-center"> Send Message{" "}<FiSend /></span>}              
             </button>
+
           </form>
         )}
       </div>
